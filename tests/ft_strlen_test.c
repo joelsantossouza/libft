f1590ba87a2ed16a0f5443d207d06eec05baaa58
joel <joelsantossouza08@gmail.com>
2025-09-25 21:38:19 +0100

    Finish all tests in ft_strlen

--- tests/ft_strlen_test.c
+++ tests/ft_strlen_test.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 12:55:34 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/25 18:57:11 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/25 21:13:51 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,11 +55,67 @@ ATF_TC_BODY(test00, tc)
 }
 
 // TEST 01
+ATF_TC(test01);
+ATF_TC_HEAD(test01, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "String of size 1");
+}
+ATF_TC_BODY(test01, tc)
+{
+	char	*test01[] = {"a", "\n", "\001", "\177", "\x0a", "\200", "", "", "Z"};
+	size_t	expected = 1;
+
+	test(test01, NELEM(test01, char *), expected);
+}
+
+// TEST 02
+ATF_TC(test02);
+ATF_TC_HEAD(test02, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "String of size 32");
+}
+ATF_TC_BODY(test02, tc)
+{
+	char	*test02[] = {
+		"W0gJvWSs0PqdrCNYo5cxIiMxd0EPXvUd",
+		"qZLNW8RwHSTdjwjigW5tocnjQ28k6rS2",
+		"hZ7jDCE1c82StgFLRztBGwFPeTAg48Bw",
+		"QN7Umc862ZGKOSlR1HO2zbmoBpH90YeZ",
+		"gKibl5vfUuwDIca8EfCJV3YQAXyL1P6t"
+	};
+	size_t	expected = 32;
+
+	test(test02, NELEM(test02, char *), expected);
+}
+
+// BONUS TEST
+#ifdef LONG_STR
+# include <string.h>
+	ATF_TC(test03);
+	ATF_TC_HEAD(test03, tc)
+	{
+		atf_tc_set_md_var(tc, "descr", "String of long size");
+	}
+	ATF_TC_BODY(test03, tc)
+	{
+		char	*test03[] = {
+			LONG_STR
+		};
+		size_t	expected = strlen(LONG_STR);
+
+		test(test03, NELEM(test03, char *), expected);
+	}
+#endif
 
 // TEST PROGRAM
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, test00);
+	ATF_TP_ADD_TC(tp, test01);
+	ATF_TP_ADD_TC(tp, test02);
+#ifdef LONG_STR
+	ATF_TP_ADD_TC(tp, test03);
+#endif
 
 	return atf_no_error();
 }
