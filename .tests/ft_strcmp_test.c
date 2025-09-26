65597a2236abef9730ef7ad3b75ad03b3261bd3e
joel <joelsantossouza08@gmail.com>
2025-09-26 10:08:40 +0100

    Writing test of empty and null strings, and realize that ft_strcmp(NULL, ) should give -1 and not 0

--- .tests/ft_strcmp_test.c
+++ .tests/ft_strcmp_test.c
@@ -6,8 +6,80 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 09:30:13 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/26 09:30:15 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/26 10:00:19 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <atf-c.h>
+#include <stdio.h>
 
+#define NELEM(tab) (sizeof(tab) / sizeof(t_input))
+#define GREEN
+#define RED
+#define RESET_COLOR
+
+/*
+	STRUCTURE -->
+		- INPUTS struct
+		- FUNCTION that compare original strcmp output with the same input as ft_strcmp
+		- A INPUT, EXPECTED and OUTPUT structure of print result
+*/
+
+char	*test_titles[] = {
+	"Empty and Nulls",
+	"Size of One",
+	"Size of Five",
+	"Non Printable",
+	"Out of Ascii Range",
+	"Long strings"
+};
+
+typedef struct s_input
+{
+	char	*s1;
+	char	*s2;
+}	t_input;
+
+void	test(t_input tab[], int size)
+{
+	static int	i;
+	int			expected;
+	int			output;
+
+	printf("<test%02d> %s\n\n----------\n", i, test_titles[i]);
+	while (size--)
+	{
+		expected = strcmp(tab[size].s1, tab[size].s2);
+		output = ft_strcmp(tab[size].s1, tab[size].s2);
+		color = output == expected ? GREEN : RED;
+
+		printf("%s", COLOR);
+		printf("Input:   \t\"%s\"\t-\t\"%s\"\n", tab[size].s1, tab[size].s2);
+		printf("Expected:\t%3d\n", expected);
+		printf("Output:  \t%3d\n", output);
+		printf("%s", RESET_COLOR);
+
+		printf("\n----------\n");
+	}
+	i++;
+}
+
+// TEST 00 --> EMPTY AND NULLS
+ATF_TC(test00);
+ATF_TC_BODY(tes00, tc)
+{
+	t_input tab[] = {
+		{"", ""}
+	};
+
+	test(tab, NELEM(tab));
+}
+
+// TEST PROGRAM
+ATF_TP_ADD_TCS(tp)
+{
+	ATF_TP_ADD_TC(tp, test00);
+
+	return (atf_no_error());
+}
--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 08:40:10 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/26 09:27:34 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/26 10:07:26 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	ft_strcmp(const char *s1, const char *s2)
 	if (!p1 && !p2)
 		return (0);
 	if (!p1)
-		return (-*p2);
+		return (-1);
 	if (!p2)
-		return (*p1);
+		return (1);
 	while (*p1 == *p2 && *p1)
 	{
 		p1++;
