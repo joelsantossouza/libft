5923b21ee1b4fef386d3d2fd1447159382e6598a
joel <joelsantossouza08@gmail.com>
2025-09-27 19:22:57 +0100

    Finish ft_strcpy, starting write his tests

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,10 +6,14 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 17:25:21 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/26 17:40:02 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/27 19:22:34 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strcpy(char *dst, const char *src)
 {
+	ft_stpcpy(dst, src);
+	return (dst);
 }
