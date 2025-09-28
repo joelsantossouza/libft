ccbf34d587068dc05ae97429b6d3853cd1a3bf1c
joel <joelsantossouza08@gmail.com>
2025-09-28 22:04:09 +0100

    finish write ft_toupper

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 10:59:12 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/28 21:11:23 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/28 21:59:52 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strnlen(const char *s, size_t maxlen);
 int		ft_strcmp(const char *s1, const char *s2);
