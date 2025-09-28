233a33dcc1bffd843d411a8e6bd0e4616b4e0250
joel <joelsantossouza08@gmail.com>
2025-09-28 20:31:05 +0100

    finish ft_isspace and have already write his tests

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 10:59:12 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/28 20:17:14 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/28 20:27:55 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 int		ft_islower(int c);
 int		ft_isupper(int c);
+int		ft_isspace(int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strnlen(const char *s, size_t maxlen);
 int		ft_strcmp(const char *s1, const char *s2);
