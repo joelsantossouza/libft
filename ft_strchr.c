e97ae412a128df8ad7148ad6e1d1049696cedd8e
joel <joelsantossouza08@gmail.com>
2025-09-29 09:15:46 +0100

    finish tests of ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,21 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 22:31:00 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/28 22:34:45 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/29 09:14:53 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
+	char	find;
+
+	find = (char) c;
 	if (s)
 	{
 		while (*s)
-			if (*s++ == c)
+			if (*s++ == find)
 				return ((char *) (s - 1));
-		if (!c)
+		if (find == 0)
 			return ((char *) s);
 	}
 	return (0);
