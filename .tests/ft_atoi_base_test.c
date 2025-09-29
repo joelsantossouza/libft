f5ac0cc69c33e182d453296816d73965044c4c33
joel <joelsantossouza08@gmail.com>
2025-09-29 15:15:20 +0100

    ft_atoi_base: Finish test00 <null inputs>

--- .tests/ft_atoi_base_test.c
+++ .tests/ft_atoi_base_test.c
@@ -6,14 +6,13 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 19:25:36 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/29 15:11:44 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/29 15:14:24 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <ctype.h>
 #include <stdio.h>
-#include <limits.h>
+#include <stdio.h>
 #include <atf-c.h>
 
 #define RED		"\e[0;31m"
