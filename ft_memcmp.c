87a52fa3a65f1943dd5a15fa520b1ab1db96b17d
joel <joelsantossouza08@gmail.com>
2025-09-29 18:04:11 +0100

    ft_memcmp: Finish code, starting tests

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:53:16 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/29 17:54:59 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/29 18:00:53 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	p1 = (const unsigned char *) s1;
 	p2 = (const unsigned char *) s2;
+	if (!n || (!p1 && !p2))
+		return (0);
+	if (!p1)
+		return (-1);
+	if (!p2)
+		return (1);
+	while (*p1 == *p2 && n-- > 1)
+	{
+		p1++;
+		p2++;
+	}
+	return (*p1 - *p2);
 }
