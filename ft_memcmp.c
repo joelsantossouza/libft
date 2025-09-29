60cae571a74465327931c9a99342c9496795fff5
joel <joelsantossouza08@gmail.com>
2025-09-29 17:56:44 +0100

    Fix my Makefile

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,34 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/29 09:46:16 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/29 17:56:37 by joesanto         ###   ########.fr       */
+/*   Created: 2025/09/29 17:53:16 by joesanto          #+#    #+#             */
+/*   Updated: 2025/09/29 17:54:59 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*p1;
 	const unsigned char	*p2;
 
 	p1 = (const unsigned char *) s1;
 	p2 = (const unsigned char *) s2;
-	if (!n || (!p1 && !p2))
-		return (0);
-	if (!p1)
-		return (-1);
-	if (!p2)
-		return (1);
-	while (*p1 == *p2 && *p1 && n-- > 1)
-	{
-		p1++;
-		p2++;
-	}
-	return (*p1 - *p2);
 }
