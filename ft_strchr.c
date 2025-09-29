86a7c7f78f98edb4d8bbcba15f314a5d0045c7cf
joel <joelsantossouza08@gmail.com>
2025-09-29 09:20:17 +0100

    fix norminette error in ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 22:31:00 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/29 09:14:53 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/29 09:19:36 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strchr(const char *s, int c)
 	{
 		while (*s)
 			if (*s++ == find)
-				return ((char *) (s - 1));
+				return ((char *)(s - 1));
 		if (find == 0)
 			return ((char *) s);
 	}
