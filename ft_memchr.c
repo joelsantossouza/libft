4b14197e1f29bdc95fb5d577c6fe58d02b5581bf
joel <joelsantossouza08@gmail.com>
2025-09-29 14:22:15 +0100

    ft_memchr: Finish writing his code, starting his tests

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,8 +6,22 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:13:04 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/29 14:13:05 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/29 14:20:39 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*p;
+	unsigned char		find;
+
+	p = (const unsigned char *) s;
+	find = (unsigned char) c;
+	if (p)
+		while (n--)
+			if (*p++ == find)
+				return ((void *)(p - 1));
+	return (0);
+}
