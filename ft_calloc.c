ffb74d1ba1d8512bfc7ff88bf2a3c51d3e74d654
joel <joelsantossouza08@gmail.com>
2025-09-30 10:40:46 +0100

    ft_calloc: Finish its code, now starting tests

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,8 +6,18 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:29:44 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/30 10:29:45 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/30 10:39:30 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	bytes;
+
+	bytes = nmemb * size;
+	if (bytes > INT_MAX)
+		return (0);
+	return (ft_memset(malloc(bytes), 0, bytes));
+}
