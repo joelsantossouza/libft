a1b7ad9b5db801bc5ed5bb56b9d3506cf33b869f
joel <joelsantossouza08@gmail.com>
2025-09-30 09:19:40 +0100

    ft_memmove: Finish writing his code, now im going to start writing his tests

---
+++ ft_memmove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 09:08:13 by joesanto          #+#    #+#             */
+/*   Updated: 2025/09/30 09:18:30 by joesanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*pdest;
+	const char	*psrc;
+
+	if (dest <= src || dest >= src + n)
+		return (ft_memcpy(dest, src, n));
+	pdest = (char *) dest;
+	psrc = (const char *) src;
+	if (pdest && psrc)
+		while (n--)
+			pdest[n] = psrc[n];
+	return (dest);
+}
