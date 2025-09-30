1c9d8c86ac19cd6bdf00e7c5e88488c9fbf7cf4f
joel <joelsantossouza08@gmail.com>
2025-09-30 16:36:41 +0100

    ft_strdup: Added the condition if s is null return null <test00> doent pass

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 13:31:51 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/30 13:50:09 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/09/30 16:35:45 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 char	*ft_strdup(const char *s)
 {
+	if (!s)
+		return (0);
 	return (ft_strcpy(malloc(sizeof(char) * (ft_strlen(s) + 1)), s));
 }
