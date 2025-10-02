bc112a3d849394c267ec4128c4e51e41104c36ba
joel <joelsantossouza08@gmail.com>
2025-10-02 12:07:43 +0100

    ft_strpbrknul_diff finish his tests

--- ft_strpbrknul_diff.c
+++ ft_strpbrknul_diff.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 23:44:58 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/02 11:34:44 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/02 12:06:13 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 char	*ft_strpbrknul_diff(const char *s, const char *reject)
 {
-	if (s && reject && *reject)
-		while (*s)
-			if (!ft_strchr(reject, *s++))
-				return ((char *)(s - 1));
+	if (!s || !reject)
+		return (0);
+	while (*s)
+		if (!ft_strchr(reject, *s++))
+			return ((char *)(s - 1));
 	return ((char *) s);
 }
