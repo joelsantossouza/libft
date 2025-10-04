0fe85f45ea685cd8a113a04c8d0e4f6801143846
joel <joelsantossouza08@gmail.com>
2025-10-04 17:55:37 +0100

    ft_strtrim: improve a little bit the implementation

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 19:03:22 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/30 19:19:54 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/04 17:54:01 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1)
 		return (0);
-	while (*s1 && ft_strchr(set, *s1))
-		s1++;
+	s1 = ft_strpbrknul_diff(s1, set);
 	end = s1 + ft_strlen(s1);
 	while (end > s1 && ft_strchr(set, *end))
 		end--;
