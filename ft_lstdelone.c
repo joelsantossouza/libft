75c51ef3d49ddfe7d06da467e033fcc928fb3ae7
joel <joelsantossouza08@gmail.com>
2025-10-07 15:00:06 +0100

    ft_lstdelone: Improving implementation

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:27:23 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/07 14:53:59 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/07 14:57:24 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	if (!lst)
 		return ;
 	if (del)
-	{
 		del(lst->content);
-		lst->content = 0;
-	}
+	lst->content = 0;
 	free(lst);
 }
