7c6d06bf4e08f4144746469e660dca8078fe27f9
joel <joelsantossouza08@gmail.com>
2025-10-08 11:38:14 +0100

    ft_memset and ft_mempset fixed

--- ft_mempset.c
+++ ft_mempset.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:53:41 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/30 13:13:55 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/08 11:37:35 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*ft_mempset(void *s, int c, size_t n)
 {
-	char	*ptr;
-	char	set;
+	unsigned char	*ptr;
+	unsigned char	set;
 
-	ptr = (char *) s;
-	set = (char) c;
+	ptr = (unsigned char *) s;
+	set = (unsigned char) c;
 	if (ptr)
 		while (n--)
 			*ptr++ = set;
