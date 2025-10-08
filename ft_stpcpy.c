8b7957411cfb912a1fa5f487c7ce444d9e288d81
joel <joelsantossouza08@gmail.com>
2025-10-08 11:51:36 +0100

    ft_stpcpy and ft_strcpy now is treat strings as unsigned

--- ft_stpcpy.c
+++ ft_stpcpy.c
@@ -6,17 +6,21 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 13:40:10 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/27 17:19:15 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/08 11:50:35 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_stpcpy(char *dst, const char *src)
 {
-	if (dst && src)
-	{
-		while (*src)
-			*dst++ = *src++;
-		*dst = 0;
-	}
-	return (dst);
+	unsigned char		*pdst;
+	const unsigned char *psrc;
+
+	if (!dst || !src)
+		return (0);
+	pdst = (unsigned char *) dst;
+	psrc = (const unsigned char *) src;
+	while (*psrc)
+		*pdst++ = *psrc++;
+	*pdst = 0;
+	return ((char *) pdst);
 }
