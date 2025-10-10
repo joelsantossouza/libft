3cf08a3571f730311eb4f58715d97e86b59eb83a
joel <joelsantossouza08@gmail.com>
2025-10-10 17:17:44 +0100

    new ft_nbrlen: finish tests

--- ft_nbrlen.c
+++ ft_nbrlen.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:00:46 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/10 17:13:20 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/10 17:15:59 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 size_t	ft_nbrlen(ssize_t nbr, int base)
 {
-	return ((nbr < 0) + ft_digit_count(nbr, base));
+	return ((nbr < 0 && base > 1) + ft_digit_count(nbr, base));
 }
