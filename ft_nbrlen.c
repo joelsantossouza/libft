1eff8ef53d33191da6ae9bb8c6bb3cc3d0aefe3e
joel <joelsantossouza08@gmail.com>
2025-10-11 09:50:05 +0100

    improve implementation of ft_nbrlen

--- ft_nbrlen.c
+++ ft_nbrlen.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:00:46 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/11 09:20:15 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/11 09:48:23 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 size_t	ft_nbrlen(ssize_t nbr, unsigned int base)
 {
-	return ((nbr < 0 && base > 1) + ft_digit_count(nbr, base));
+	size_t	len;
+
+	if (base < 2)
+		return (0);
+	len = 1 + (nbr < 0);
+	while (nbr / base)
+	{
+		nbr /= base;
+		len++;
+	}
+	return (len);
 }
