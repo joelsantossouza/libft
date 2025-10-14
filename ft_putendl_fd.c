5b12e437e262cd9686f1a53d794956d727caa8c8
joel <joelsantossouza08@gmail.com>
2025-10-14 18:59:16 +0100

    finish changes in ft_putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,13 +6,13 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 17:04:59 by joesanto          #+#    #+#             */
-/*   Updated: 2025/09/30 17:14:08 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/14 18:58:22 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(const char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
 	ft_putstr_fd("\n", fd);
