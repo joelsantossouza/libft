b09b058324af7d4d137190de61a6ae13534b71e7
joel <joelsantossouza08@gmail.com>
2025-10-14 08:43:06 +0100

    fix ft_strrchr redundante include of libft

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,9 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 09:29:05 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/09 18:22:54 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/14 08:42:41 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
