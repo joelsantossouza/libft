8b44c1c527ab895ed6bf04547ee12a428e012e17
joel <joelsantossouza08@gmail.com>
2025-10-29 10:11:22 +0000

    faster memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 09:08:13 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/08 11:23:47 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:11:04 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,18 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	if (dest <= src)
 		return (ft_memcpy(dest, src, n));
+	if (!dest || !src)
+		return (0);
 	pdest = (unsigned char *) dest;
 	psrc = (const unsigned char *) src;
-	if (pdest && psrc)
-		while (n--)
-			pdest[n] = psrc[n];
+	while (n >= 8)
+	{
+		n -= 8;
+		((size_t *) pdest)[n] = ((size_t *) psrc)[n];
+		pdest -= 8;
+		psrc -= 8;
+	}
+	while (n--)
+		pdest[n] = psrc[n];
 	return (dest);
 }
