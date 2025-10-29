87f398956e3d82f1d37a0b453a412f3bc72c4d28
joel <joelsantossouza08@gmail.com>
2025-10-29 09:13:50 +0000

    improve ft_realloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 18:49:08 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/24 19:29:27 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/29 09:13:09 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,10 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 
 	new = 0;
 	if (new_size)
-		new = ft_memcpy(malloc(new_size), ptr, ft_min(old_size, new_size));
+	{
+		new = malloc(new_size);
+		ft_memcpy(new, ptr, ft_min(old_size, new_size));
+	}
 	free(ptr);
 	return (new);
 }
