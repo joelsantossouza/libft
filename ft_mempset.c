eeddfae5dc7488b50e3e22ac5cfbbf4548a56bdb
joel <joelsantossouza08@gmail.com>
2025-10-29 11:11:48 +0000

    faster mempset and memset and bzero

--- ft_mempset.c
+++ ft_mempset.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:53:41 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/08 11:37:35 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/29 11:08:57 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@
 void	*ft_mempset(void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
-	unsigned char	set;
+	unsigned char	set_chr;
+	size_t			set_block;
 
+	if (!s)
+		return (0);
 	ptr = (unsigned char *) s;
-	set = (unsigned char) c;
-	if (ptr)
-		while (n--)
-			*ptr++ = set;
+	set_chr = (unsigned char) c;
+	set_block = (size_t) set_chr * 0x0101010101010101UL;
+	while (n >= 8)
+	{
+		*(size_t *) ptr = set_block;
+		ptr += 8;
+		n -= 8;
+	}
+	while (n--)
+		*ptr++ = set_chr;
 	return ((void *) ptr);
 }
