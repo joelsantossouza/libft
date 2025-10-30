1cb7a4055ec29b69693331a862f96f2a1d0c64be
joel <joelsantossouza08@gmail.com>
2025-10-30 21:33:10 +0000

    Improve security of get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 20:35:51 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/30 12:04:51 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/30 21:32:10 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,7 @@ ssize_t	get_next_line(char **lineptr, int fd)
 		if (*(ptr - 1) == '\n')
 			break ;
 	}
+	if (len < 0)
+		*lineptr = ft_realloc(*lineptr, 0, 0);
 	return (len);
 }
