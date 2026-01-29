20e39e8f2d915cbe33450e04b1b76d95edba6bab
joel <joelsantossouza08@gmail.com>
2026-01-29 13:03:42 +0000

    Fix undefined behaviour of ft_memcpy doing the alignment of bytes first

--- .tests/ft_memcpy_test.c
+++ .tests/ft_memcpy_test.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 18:09:36 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/01 15:58:22 by joesanto         ###   ########.fr       */
+/*   Updated: 2026/01/29 12:33:51 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -288,6 +288,192 @@ ATF_TC_BODY(test05, tc)
 	test(tab, NELEM(tab));
 }
 
+ATF_TC(test_06_unaligned_destination);
+ATF_TC_HEAD(test_06_unaligned_destination, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test copying to unaligned destination");
+}
+ATF_TC_BODY(test_06_unaligned_destination, tc)
+{
+	char buffer[30];
+	const char *src = "this is a test";
+
+	memset(buffer, 'X', sizeof(buffer));
+	ft_memcpy(buffer + 13, src, strlen(src));
+
+	ATF_REQUIRE_EQ(memcmp(buffer + 13, src, strlen(src)), 0);
+	// Verify data before offset wasn't touched
+	for (int i = 0; i < 13; i++)
+		ATF_REQUIRE_EQ(buffer[i], 'X');
+}
+
+ATF_TC(test_07_unaligned_source);
+ATF_TC_HEAD(test_07_unaligned_source, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test copying from unaligned source");
+}
+ATF_TC_BODY(test_07_unaligned_source, tc)
+{
+	char source[30] = "XXXXXXXXXXhello world";
+	char dest[15];
+
+	ft_memcpy(dest, source + 10, 11);
+
+	ATF_REQUIRE_EQ(memcmp(dest, "hello world", 11), 0);
+}
+
+ATF_TC(test_08_both_unaligned);
+ATF_TC_HEAD(test_08_both_unaligned, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test copying between two unaligned pointers");
+}
+ATF_TC_BODY(test_08_both_unaligned, tc)
+{
+	char source[30] = "AAAAA12345BBBBB";
+	char dest[30];
+
+	memset(dest, 'X', sizeof(dest));
+	ft_memcpy(dest + 7, source + 5, 5);
+
+	ATF_REQUIRE_EQ(memcmp(dest + 7, "12345", 5), 0);
+	// Verify surrounding data wasn't touched
+	for (int i = 0; i < 7; i++)
+		ATF_REQUIRE_EQ(dest[i], 'X');
+}
+
+ATF_TC(test_09_unaligned_odd_offset);
+ATF_TC_HEAD(test_09_unaligned_odd_offset, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test with odd offset (misalignment)");
+}
+ATF_TC_BODY(test_09_unaligned_odd_offset, tc)
+{
+	char buffer[50];
+	const char *data = "testing unaligned copy";
+
+	memset(buffer, 0, sizeof(buffer));
+	ft_memcpy(buffer + 3, data, strlen(data));
+
+	ATF_REQUIRE_EQ(memcmp(buffer + 3, data, strlen(data)), 0);
+}
+
+ATF_TC(test_10_unaligned_various_sizes);
+ATF_TC_HEAD(test_10_unaligned_various_sizes, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test unaligned copies with various sizes");
+}
+ATF_TC_BODY(test_10_unaligned_various_sizes, tc)
+{
+	char buffer[100];
+
+	// 1 byte unaligned
+	memset(buffer, 0, sizeof(buffer));
+	ft_memcpy(buffer + 5, "A", 1);
+	ATF_REQUIRE_EQ(buffer[5], 'A');
+
+	// 3 bytes unaligned
+	memset(buffer, 0, sizeof(buffer));
+	ft_memcpy(buffer + 7, "ABC", 3);
+	ATF_REQUIRE_EQ(memcmp(buffer + 7, "ABC", 3), 0);
+
+	// 17 bytes unaligned (crosses alignment boundaries)
+	memset(buffer, 0, sizeof(buffer));
+	ft_memcpy(buffer + 9, "1234567890ABCDEFG", 17);
+	ATF_REQUIRE_EQ(memcmp(buffer + 9, "1234567890ABCDEFG", 17), 0);
+}
+
+ATF_TC(test_11_unaligned_end_of_buffer);
+ATF_TC_HEAD(test_11_unaligned_end_of_buffer, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test copying to near end of buffer");
+}
+ATF_TC_BODY(test_11_unaligned_end_of_buffer, tc)
+{
+	char buffer[20];
+	const char *src = "TEST";
+
+	memset(buffer, 'X', sizeof(buffer));
+	ft_memcpy(buffer + 16, src, 4);
+
+	ATF_REQUIRE_EQ(memcmp(buffer + 16, "TEST", 4), 0);
+}
+
+ATF_TC(test_12_unaligned_single_byte_offset);
+ATF_TC_HEAD(test_12_unaligned_single_byte_offset, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test with single byte offset");
+}
+ATF_TC_BODY(test_12_unaligned_single_byte_offset, tc)
+{
+	char buffer[30];
+	const char *data = "alignment test data";
+
+	memset(buffer, 0, sizeof(buffer));
+	ft_memcpy(buffer + 1, data, strlen(data));
+
+	ATF_REQUIRE_EQ(buffer[0], 0);
+	ATF_REQUIRE_EQ(memcmp(buffer + 1, data, strlen(data)), 0);
+}
+
+ATF_TC(test_13_unaligned_crossing_boundaries);
+ATF_TC_HEAD(test_13_unaligned_crossing_boundaries, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test unaligned copy crossing word boundaries");
+}
+ATF_TC_BODY(test_13_unaligned_crossing_boundaries, tc)
+{
+	char source[40];
+	char dest[40];
+
+	// Fill source with pattern
+	for (size_t i = 0; i < sizeof(source); i++)
+		source[i] = 'A' + (i % 26);
+
+	memset(dest, 0, sizeof(dest));
+
+	// Copy from offset 3 to offset 5, 20 bytes
+	ft_memcpy(dest + 5, source + 3, 20);
+
+	ATF_REQUIRE_EQ(memcmp(dest + 5, source + 3, 20), 0);
+}
+
+ATF_TC(test_14_unaligned_all_offsets);
+ATF_TC_HEAD(test_14_unaligned_all_offsets, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test all possible single-byte offsets");
+}
+ATF_TC_BODY(test_14_unaligned_all_offsets, tc)
+{
+	char buffer[50];
+	const char *pattern = "0123456789ABCDEF";
+	size_t pattern_len = strlen(pattern);
+
+	// Test offsets 0-15
+	for (int offset = 0; offset < 16; offset++)
+	{
+		memset(buffer, 'X', sizeof(buffer));
+		ft_memcpy(buffer + offset, pattern, pattern_len);
+		ATF_REQUIRE_EQ(memcmp(buffer + offset, pattern, pattern_len), 0);
+	}
+}
+
+ATF_TC(test_15_unaligned_binary_data);
+ATF_TC_HEAD(test_15_unaligned_binary_data, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Test unaligned copy with binary data (including nulls)");
+}
+ATF_TC_BODY(test_15_unaligned_binary_data, tc)
+{
+	unsigned char source[20] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+		0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+	unsigned char dest[30];
+
+	memset(dest, 0, sizeof(dest));
+	ft_memcpy(dest + 11, source, 16);
+
+	ATF_REQUIRE_EQ(memcmp(dest + 11, source, 16), 0);
+}
+
 // TEST PROGRAM
 ATF_TP_ADD_TCS(tp)
 {
@@ -297,6 +483,16 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, test03);
 	ATF_TP_ADD_TC(tp, test04);
 	ATF_TP_ADD_TC(tp, test05);
+	ATF_TP_ADD_TC(tp, test_06_unaligned_destination);
+    ATF_TP_ADD_TC(tp, test_07_unaligned_source);
+    ATF_TP_ADD_TC(tp, test_08_both_unaligned);
+    ATF_TP_ADD_TC(tp, test_09_unaligned_odd_offset);
+    ATF_TP_ADD_TC(tp, test_10_unaligned_various_sizes);
+    ATF_TP_ADD_TC(tp, test_11_unaligned_end_of_buffer);
+    ATF_TP_ADD_TC(tp, test_12_unaligned_single_byte_offset);
+    ATF_TP_ADD_TC(tp, test_13_unaligned_crossing_boundaries);
+    ATF_TP_ADD_TC(tp, test_14_unaligned_all_offsets);
+    ATF_TP_ADD_TC(tp, test_15_unaligned_binary_data);
 
 	return (atf_no_error());
 }
--- ft_mempcpy.c
+++ ft_mempcpy.c
@@ -6,21 +6,28 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 17:48:26 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/29 09:57:53 by joesanto         ###   ########.fr       */
+/*   Updated: 2026/01/29 13:02:25 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdint.h>
 
 void	*ft_mempcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char		*pdest;
 	const unsigned char	*psrc;
+	const size_t		ptr_mask = sizeof(uintptr_t) - 1;
 
 	if (!dest || !src)
 		return (0);
 	pdest = (unsigned char *) dest;
 	psrc = (const unsigned char *) src;
+	while (n > 0 && (((uintptr_t)pdest | (uintptr_t)psrc) & ptr_mask))
+	{
+		*pdest++ = *psrc++;
+		n--;
+	}
 	while (n >= 8)
 	{
 		*(size_t *) pdest = *(size_t *) psrc;
@@ -28,7 +35,7 @@ void	*ft_mempcpy(void *dest, const void *src, size_t n)
 		psrc += 8;
 		n -= 8;
 	}
-	while (n--)
+	while (n-- > 0)
 		*pdest++ = *psrc++;
 	return ((void *) pdest);
 }
